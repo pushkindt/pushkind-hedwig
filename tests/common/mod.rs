10dea5bd4509dff83640ebfc11fd41e9d6f9dc8a
Viacheslav Matrizaev <matrizaev@gmail.com>
2025-09-25 23:12:01 +0300

    used TestDb for testing

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -5,7 +5,7 @@ use pushkind_common::db::{DbPool, establish_connection_pool};
 /// Temporary database used in integration tests.
 pub struct TestDb {
     filename: String,
-    pool: DbPool,
+    pool: Option<DbPool>,
 }
 
 impl TestDb {
@@ -20,17 +20,24 @@ impl TestDb {
             .expect("Failed to get SQLite connection from pool.");
         TestDb {
             filename: filename.to_string(),
-            pool,
+            pool: Some(pool),
         }
     }
     #[allow(dead_code)]
     pub fn pool(&self) -> DbPool {
-        self.pool.clone()
+        self.pool
+            .as_ref()
+            .expect("TestDb pool should be available")
+            .clone()
     }
 }
 
 impl Drop for TestDb {
     fn drop(&mut self) {
+        if let Some(pool) = self.pool.take() {
+            drop(pool);
+        }
+
         std::fs::remove_file(&self.filename).ok();
         std::fs::remove_file(format!("{}-shm", &self.filename)).ok();
         std::fs::remove_file(format!("{}-wal", &self.filename)).ok();
--- tests/db.rs
+++ tests/db.rs
@@ -6,7 +6,8 @@ fn test_creates_and_removes_db_files() {
 
     {
         let test_db = common::TestDb::new(base);
-        let conn = test_db.pool().get();
+        let pool = test_db.pool();
+        let conn = pool.get();
         assert!(conn.is_ok());
     }
 
